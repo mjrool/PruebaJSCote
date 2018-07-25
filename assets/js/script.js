b82f612e1ebfc946202aea8032b871e05cb23c24
MariaJose <mjroolma@gmail.com>
2018-07-25 07:20:08 -0400

    avance js ingreso formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,36 @@
 
 $(function(){
 
+
+	$('#user__name').focus();
+	$('form').submit(function (event){
+		event.preventDefault();
+		var formData = {
+			userName: $('#user__name').val(),
+			userComentario: $('#comentario__text').val(),
+		}
+
+		postCreate(formData);
+		function postCreate(formData) {
+			var post = $('.col-derecha:first').clone().css("display","block");
+			post.find('.name__tweet').text(formData.userName);
+			post.find('.comentario__tweet').text(formData.userComentario);
+			var file = document.getElementById('imagen__user').files[0];
+			var reader = new FileReader();
+			reader.onloadend = function (){
+				post.find('.photo__tweet').attr('src,reader.result');
+			}
+			if (file){
+				reader.readAsDataURL(file);
+			} else {
+				swal('Selecciona una imagen');
+				return;
+			}
+			$('.row').after(post);
+			$('.').attr('src','' );
+		}
+	})	
+
 // CORAZON LIKE	
 
 	$('.new-tweet').on('click', '.icon-heart-tweet', function(e){
